e5e0e935caf48aef6cbc4e57148dad72ee4d2093
Laurenz Hill <hill.laurenz@campus.lmu.de>
2021-02-24 15:12:24 +0100

    Initial Commit

---
+++ LaurenzCodingChallangeEnvidual/Model/Repository.swift
@@ -0,0 +1,19 @@
+//
+//  Repository.swift
+//  LaurenzCodingChallangeEnvidual
+//
+//  Created by Lori Hill on 24.02.21.
+//
+
+import Foundation
+
+struct Repository: Codable {
+    
+    let id:Int
+    let name: String
+    let full_name: String
+    let description: String
+    let stargazers_count: Int
+    let updated_at: String
+    
+}
---
+++ LaurenzCodingChallangeEnvidual/View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LaurenzCodingChallangeEnvidual
+//
+//  Created by Lori Hill on 24.02.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
